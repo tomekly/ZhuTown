6e68bae01185e4ae84e857d9a1598b01561dcabf
Hayquan <799689663@qq.com>
2016-12-13 10:59:36 +0800

    删除测试代码

--- app/src/main/java/com/dongbang/yutian/retrofit/RetrofitFactory.java
+++ app/src/main/java/com/dongbang/yutian/retrofit/RetrofitFactory.java
@@ -1,9 +1,7 @@
 package com.dongbang.yutian.retrofit;
 
 import retrofit2.Retrofit;
-
 import retrofit2.converter.gson.GsonConverterFactory;
-import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 
 /**
